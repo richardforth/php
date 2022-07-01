fb900dadda895215227ef3310eef0ad76f82e541
Richard A. Forth <richard.forth@gmail.com>
2022-07-01 21:48:42 +0000

    added comparison_operators.php

---
+++ UDEMY_Learn_PHP_from_Scratch/Section_02/comparison_operators.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+
+==   equal to 
+!=   not equal to
+>    greater than
+<    less than
+>=   greater than or equal to
+<=   less than or equal to
+<>   not equal to (or "less than or greater than")
+
+*/
+
+
+$name = "Richard";
+$age = 43;
+
+if ($name=="Richard") {
+  echo "TRUE.";
+} else {
+  echo "NOT TRUE.";
+}
+
+echo "<hr>";
+
+if ($age>=22) {
+  echo "TRUE.";
+} else {
+  echo "NOT TRUE.";
+}
+
+
+
+?>
