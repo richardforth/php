437991fd1f6c013f49b38c849d7efd353fa2148b
Richard A. Forth <richard.forth@gmail.com>
2022-07-01 02:10:01 +0000

    Update echo.php

--- UDEMY_Learn_PHP_from_Scratch/Section_1/echo.php
+++ UDEMY_Learn_PHP_from_Scratch/Section_1/echo.php
@@ -1,4 +1,10 @@
 <?php
+  echo 'Hello World!<br />'; // single quotes
+  echo "Hello World!<br />"; // double quotes
+  echo "Hello World!\n"; // backslash n does not work here
+  // but it will work on the CLI, unlike the <br /> tags which are html
+  // also shows up in page source  
   echo 'This is data output to the browser';
-
+  echo '<br>Hello World!<br>It\'s a lovely day.';
+  print '<br>Hello World!<br>It\'s a lovely day.'; // print is slower than echo
 ?>
