998983009f8de70f1ce50ad0862547b4b423ab6d
Richard A. Forth <richard.forth@gmail.com>
2022-07-01 01:56:30 +0000

    Add comments.php and move index to first.php

---
+++ UDEMY_Learn_PHP_from_Scratch/Section_1/comments.php
@@ -0,0 +1,14 @@
+<?php
+
+// this is a function
+// that takes a string
+// and converts it to upper case
+
+/*
+** This is a function
+** that takes a string
+** and converts it to upper case
+*/
+
+
+echo 'Richard A. Forth'; // Output my name.
