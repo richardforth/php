fe06f6892d990597adc3a99b1aee041e4ce7010a
Richard Forth <richard.forth@gmail.com>
2022-07-01 02:18:24 +0100

    Update index.php

--- UDEMY_Learn_PHP_from_Scratch/Section_1/index.php
+++ UDEMY_Learn_PHP_from_Scratch/Section_1/index.php
@@ -1,11 +1,19 @@
-<?php
-  echo 'Test';
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>My Title</title>
+   </head>
+  <body>
+    <?php
+      echo 'Test';
 
-  for($x = 1, $x <= 10; $x++) {
-       echo "{$x}";
-       if($x != 10) {
-            echo ", "; 
-       }
-  }
+      for($x = 1, $x <= 10; $x++) {
+        echo "{$x}";
+        if($x != 10) {
+          echo ", "; 
+        }
+      }
   
-?>
+    ?>
+    </body>
+</html>
