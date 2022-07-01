9e0afbc772f09c922c266ea70733b655939859e1
Richard A. Forth <richard.forth@gmail.com>
2022-07-01 20:34:07 +0000

    Moved files about

--- UDEMY_Learn_PHP_from_Scratch/Section_02/if_statements.php
+++ UDEMY_Learn_PHP_from_Scratch/Section_02/if_statements.php
@@ -10,4 +10,19 @@ if ($password == 'alex') {
 }
 
 
+echo "<hr>";
+
+$age=11;
+
+
+if ($age>=21) {
+  echo 'You\'re old enough to drink in USA.';
+} else if ($age>=18) { 
+
+  echo 'You\'re old enough to drink in the UK.';
+} else {
+  echo 'You\'re not old enough to drink in USA or the UK.';
+}
+
+
 ?>
