b620a26d75ed3f9107d06faef27c6276fc745763
Richard A. Forth <richard.forth@gmail.com>
2022-07-01 19:50:35 +0000

    add variables.php

---
+++ UDEMY_Learn_PHP_from_Scratch/Section_1/variables.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = "Richard";
+$age = 43;
+
+// Old Style
+echo 'Hello, my name is '.$name.' and I am '.$age.' years old.';
+
+// New (better / easier) style
+echo "<br />Hello, my name is $name and I am $age years old.";
+
+?>
