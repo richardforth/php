a5a439c373370b0516e77b9b1340e086731e1fae
Richard A. Forth <richard.forth@gmail.com>
2022-07-01 23:19:48 +0000

    Added a different switch statement.

---
+++ UDEMY_Learn_PHP_from_Scratch/Section_02/switch_statement_2.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = 'Dale';
+
+switch ($name) {
+  case 'Alex':
+  case 'Billy':
+  case 'Josh':
+    echo 'Hello there!';
+  break;
+
+ 
+  default:
+    echo "Go away.";
+  break;
+}
+
+
+?>
