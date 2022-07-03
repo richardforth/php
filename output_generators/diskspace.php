3fba1b1d9bf90cfeb235620a66af839418d05f75
Richard A. Forth <richard.forth@gmail.com>
2022-07-03 13:38:12 +0000

    Upgared prefab, added additional escape sequences to fix bugs.

--- output_generators/diskspace.php
+++ output_generators/diskspace.php
@@ -19,11 +19,11 @@ echo -e "\\nLargest Files and Directories:\\n==============================\\n";
 echo -e "Report Generated: `date`\\n";\
 df -h \$FS; \
 echo -e "\\nLargest Directories:\\n====================\\n"; \
-du -x \$FS 2>/dev/null| sort -rnk1| head -n \$NUMRESULTS| awk '{printf "%d MB %s\n",\
+du -x \$FS 2>/dev/null| sort -rnk1| head -n \$NUMRESULTS| awk '{printf "%d MB %s\\n",\
 $1/1024,$2}';\
 echo -e "\\nLargest Files:\\n==============\\n";\
 nice -n 19 find \$FS -mount -type f -ls \
-2>/dev/null| sort -rnk7| head -n \$NUMRESULTS|awk '{printf "%d MB\t%s\n",\
+2>/dev/null| sort -rnk7| head -n \$NUMRESULTS|awk '{printf "%d MB\\t%s\\n",\
 ($7/1024)/1024,\$NF}';\
 echo -e "\\nReport Finished: `date`\\n"
 }
