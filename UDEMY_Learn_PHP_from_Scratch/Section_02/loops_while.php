785fe79cd43d04f268f7e9590d9ae4ba7a87025f
Richard A. Forth <richard.forth@gmail.com>
2022-07-03 08:10:59 +0000

    Added a second while looping style.

--- UDEMY_Learn_PHP_from_Scratch/Section_02/loops_while.php
+++ UDEMY_Learn_PHP_from_Scratch/Section_02/loops_while.php
@@ -9,4 +9,15 @@ while ($num>=0) {
     $num--;
 }
 
+
+/* Another way to write a while loop is with while endwhile */
+/* Here we count down from 30 to 0 */
+$num = 30;
+
+while ($num>=0):
+    echo $num.'<br>';
+    $num--;
+endwhile
+
+
 ?>
