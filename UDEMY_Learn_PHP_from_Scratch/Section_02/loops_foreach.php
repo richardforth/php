4e3ecc621b0ff78ed615857e4a2a9bb57c7f8ee3
Richard A. Forth <richard.forth@gmail.com>
2022-07-03 09:08:09 +0000

    Added loops_foreach.php

---
+++ UDEMY_Learn_PHP_from_Scratch/Section_02/loops_foreach.php
@@ -0,0 +1,36 @@
+<?php
+
+/* foreach loops iterate over a collection of itmes, for example an array of names */
+
+$names = array('Alex', 'Billy','Dale');
+
+/* originally the tutor used $value, but $value does not self-document */
+foreach($names as $name) {
+    echo "$name<br>";
+}
+echo "<hr>";
+
+
+
+/* previously we saw us looping through a kind of dict array structure type by modifying the array */
+/* now we can loop trhough that struct without modifying! (see arrays.php for what we did previously)*/
+
+$ageOf = array('Alex'=>21,'Billy'=>16,'Dale'=>49);
+
+/* key and value works here because its similat to a dict type */
+foreach($ageOf as $key => $value) {
+   echo "$key is $value years old.<br>";
+}
+
+echo "<hr>";
+
+
+/* But as you know I like self-documenting code */
+/* lets swap $key for $name */
+/* lets swap $value for $age */
+foreach($ageOf as $name => $age) {
+    echo "$name is $age years old.<br>";
+ }
+// now we can see much more clearly what's going on.
+
+?>
