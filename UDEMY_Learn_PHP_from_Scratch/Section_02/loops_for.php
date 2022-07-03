f8c5e47a141a70aba66c5398f3b9fa032134f09e
Richard A. Forth <richard.forth@gmail.com>
2022-07-03 08:27:40 +0000

    Added comment linking to C for loop for comparison.

--- UDEMY_Learn_PHP_from_Scratch/Section_02/loops_for.php
+++ UDEMY_Learn_PHP_from_Scratch/Section_02/loops_for.php
@@ -1,6 +1,7 @@
 <?php
 
 /* I noticed that PHP for loops have a similar structure to C for loops */
+/*  - see https://github.com/richardforth/C/blob/main/for_loop.c for comparison*/
 
 $somevariable = 10;
 for ($i = 1; $i <= $somevariable; $i++) {
