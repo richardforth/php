3886194b408de5122e669309736cfa2fa0062bc7
Richard A. Forth <richard.forth@gmail.com>
2022-07-04 18:58:42 +0000

    Added implode_manual.php

---
+++ UDEMY_Learn_PHP_from_Scratch/Section_03/implode_manual.php
@@ -0,0 +1,34 @@
+<?php
+
+echo "String:<br>";
+$likes =  'eating,drinking,the gym,pizza';
+echo "<pre>";
+echo $likes;
+echo "</pre><br>";
+
+$likes = explode(',', $likes);
+
+echo "Exploded string into array:<br><br>";
+
+echo "<pre>";
+print_r($likes);
+echo "</pre>";
+
+
+// implode an array into a string - the manual way
+$likes_str = null;
+foreach ($likes as $key => $like) {
+    $likes_str .= $like;
+    if ($key != (count($names) -1)) {
+        $likes_str .= ',';
+    } 
+}
+
+echo "Imploded back to a string the manual way:<br>";
+$likes =  'eating,drinking,the gym,pizza';
+echo "<pre>";
+echo $likes;git add implode_man 
+echo "</pre><br>"
+
+
+?>
