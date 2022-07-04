2f93eecb1cda8c85eeb8ae30478315a53d31d8d7
Richard A. Forth <richard.forth@gmail.com>
2022-07-04 19:41:09 +0000

    Added generate_random_number.php

---
+++ UDEMY_Learn_PHP_from_Scratch/Section_03/generate_random_number.php
@@ -0,0 +1,20 @@
+<?php
+
+// Completely random number
+$number1 = rand();
+echo $number1.'<br>';
+
+//Random number between 1 and 6
+$number2 = rand(1,6);
+echo $number2.'<br>';
+
+// random number from 1 - 100 
+$number3 = rand(1,100);
+echo $number3.'<br>';
+
+// random number from 1 - 9999 
+$number4 = rand(1,9999);
+echo $number4.'<br>';
+
+
+?>
