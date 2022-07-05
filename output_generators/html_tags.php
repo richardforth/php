865b73c8af99c3f569a2fb12988a745ddacf8f88
Richard A. Forth <richard.forth@gmail.com>
2022-07-05 20:21:12 +0000

    Updated

--- output_generators/html_tags.php
+++ output_generators/html_tags.php
@@ -1,6 +1,8 @@
 <textarea rows="10" cols="30"><?php 
-echo '<html>
+echo '<!DOCTYPE html>
+<html lang="en">
     <head>
+        <meta charset="UTF-8">
         <title>My Title</title>
     </head>
     <body>
