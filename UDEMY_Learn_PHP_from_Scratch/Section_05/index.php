64a9b33b10405dee5f8d13c4c3ace5f0a27aedc3
Richard A. Forth <richard.forth@gmail.com>
2022-07-06 19:23:36 +0000

    Added a view to pull back multiple rows of data

--- UDEMY_Learn_PHP_from_Scratch/Section_05/index.php
+++ UDEMY_Learn_PHP_from_Scratch/Section_05/index.php
@@ -25,6 +25,52 @@ insert into users(email, first_name, last_name) values('marge@simpsons.com','Mar
 // 4. database_name
 $db = new mysqli('localhost','mysqli', 'mysqli_password_goes_here', 'mysqli');
 
-echo '<pre>', var_dump($db), '</pre>';
+/* good idea to save the below into a new file and include it on every page that needs the database connection */
+if ($db->connect_errno !== 0) {
+    die("We are currently down, please try again soon.");
+}
 
+// eg: require_once 'db.php';
+
+//echo '<pre>', var_dump($db), '</pre>';
+/*
+$user = $db->query("SELECT * FROM users WHERE id = 1");
+echo '<pre>', var_dump($user), '</pre>';
+echo "<hr>";
+$user = $user->fetch_assoc();
+echo '<pre>', var_dump($user), '</pre>';
+echo "<hr>";
+echo $user["email"];
+echo "<hr>";
+$user = $db->query("SELECT * FROM users WHERE id = 2");
+$user = $user->fetch_assoc();
+echo $user["email"];
+echo "<hr>";
+$user = $db->query("SELECT * FROM users WHERE id = 3");
+$user = $user->fetch_assoc();
+echo $user["email"];
+echo "<hr>";
+$user = $db->query("SELECT * FROM users WHERE id = 4");
+$user = $user->fetch_assoc();
+echo $user["email"];
+echo "<hr>";
+*/
+//$users = $db->query("SELECT * FROM users");
+$users = $db->query("SELECT email, created, CONCAT(first_name, ' ', last_name) as full_name FROM users");
+//while ($row = $users->fetch_assoc()) {
+//   echo "<pre>", print_r($row), "</pre>";
+//}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>MYSQLI</title>
+    </head>
+    <body>
+        <?php while ($row = $users->fetch_assoc()): ?>
+            <p><?php echo $row['full_name']; ?>, <?php echo $row['email']; ?>, <?php echo $row['created']; ?></p>
+        <?php endwhile; ?>
+    </body>
+</html>
