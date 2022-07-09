9da0ed4c8f4ec8eaa7c748273650e6236c7e8bcb
Richard A. Forth <richard.forth@gmail.com>
2022-07-09 12:11:00 +0000

    Updated escaping.php

--- UDEMY_Learn_PHP_from_Scratch/Section_05/escaping.php
+++ UDEMY_Learn_PHP_from_Scratch/Section_05/escaping.php
@@ -5,13 +5,12 @@ ini_set('display_errors', 'On');
 $db = new mysqli('localhost','mysqli', 'mysqli_password_goes_here', 'mysqli');
 
 if (!empty($_POST)) {
-    $email = $_POST['email'];
-    $firstName = $_POST['first_name'];
-    $lastName = $_POST['last_name'];
+    $email = $db->escape_string($_POST['email']);
+    $firstName = $db->escape_string($_POST['first_name']);
+    $lastName = $db->escape_string($_POST['last_name']);
 
     $sql = "INSERT INTO users (email, first_name, last_name, created) VALUES ('{$email}', '{$firstName}', '{$lastName}', NOW())";
 
-    //$insert = $db->real_escape_string("
     $insert = $db->query($sql) or trigger_error("Query Failed! SQL: <br><br>$sql<br><br>Error: ". mysqli_error($db), E_USER_ERROR);
     
 
