ee47fd9239957f20d548512450046d3bd390075e
Richard Forth <richard.forth@gmail.com>
2022-07-09 11:56:55 +0100

    Update tasks.sql

--- converters/json-array-myql/tasks.sql
+++ converters/json-array-myql/tasks.sql
@@ -1,1 +1,15 @@
-sql to be added that generates the table ready for the import
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=86 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `task_status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
