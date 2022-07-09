ce84d53483fe5607c1f01e0d504cba45bb396b42
Richard Forth <richard.forth@gmail.com>
2022-07-09 11:59:53 +0100

    Update tasks.sql

--- converters/json-array-myql/tasks.sql
+++ converters/json-array-myql/tasks.sql
@@ -13,3 +13,11 @@ CREATE TABLE `task_status` (
   `status` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+-- set up the statuses in the lookup table
+insert into task_status (status) values ('TO-DO');
+insert into task_status (status) values ('IN-PROGRESS');
+insert into task_status (status) values ('ON-HOLD');
+insert into task_status (status) values ('DELAYED');
+insert into task_status (status) values ('CANCELLED');
+insert into task_status (status) values ('COMPLETED');
