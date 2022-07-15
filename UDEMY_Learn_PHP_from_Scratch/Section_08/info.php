da8f4191fe99ada740475a2ac20ad0bb178f7549
Richard A. Forth <richard.forth@gmail.com>
2022-07-15 22:00:08 +0000

    Added info.php

---
+++ UDEMY_Learn_PHP_from_Scratch/Section_08/info.php
@@ -0,0 +1,9 @@
+<?php
+// Dont leave files like this in your document root especially in production, they give a wealth of information away about your php installation
+// and server
+// Other common filenames people search for include:
+//      info.php
+//      phpinfo.php
+//      php_info.php
+  phpinfo();
+?>
