53c943eab4506d9a5bc8b3115c24599bfd17c560
Richard A. Forth <richard.forth@gmail.com>
2022-07-15 20:48:56 +0000

    Modified the isset criteria to check for  only

---
+++ UDEMY_Learn_PHP_from_Scratch/Section_07/undefined_index_fixed_v3.php
@@ -0,0 +1,20 @@
+<?php
+// checking for $_POST as a shortcut
+if (isset($_POST)) {
+    $something = $_POST['something'];
+    $something_else = $_POST['something_else'];
+
+    echo $something, ' ',  $something_else;
+}
+// this is still probably good practice 
+
+// tested the broken version of the page in php 8.0 and it doesnt throw any errors
+// course work was for php version 5.4
+?>
+
+<form action="" method="post">
+    Type something:
+    <input type="text" name ="something">
+    <input type="text" name ="something_else">
+    <input type="submit" value ="Submit">
+</form>
