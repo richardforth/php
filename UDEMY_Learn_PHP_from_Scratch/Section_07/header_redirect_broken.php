e37b6d9a4f0f78d80df920cc7b30e6308e168c9d
Richard A. Forth <richard.forth@gmail.com>
2022-07-15 20:36:06 +0000

    updated

--- UDEMY_Learn_PHP_from_Scratch/Section_07/header_redirect_broken.php
+++ UDEMY_Learn_PHP_from_Scratch/Section_07/header_redirect_broken.php
@@ -1,6 +1,7 @@
 <h4>Hello world!</h4>
 
 <?php
+
 // having output before the header redirect was supposed to break in php 5.4,
 // producing a "Cannot modify headers" error message,  but when I tested this
 // in php 8.0.12 this is actually working for me.
