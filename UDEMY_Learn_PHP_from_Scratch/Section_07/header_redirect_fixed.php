12c4a04c146f096f7f82306fb223e0066f31d24d
Richard A. Forth <richard.forth@gmail.com>
2022-07-15 20:10:33 +0000

    Updated commentary

--- UDEMY_Learn_PHP_from_Scratch/Section_07/header_redirect_fixed.php
+++ UDEMY_Learn_PHP_from_Scratch/Section_07/header_redirect_fixed.php
@@ -9,6 +9,6 @@ ob_start(); // start output buffering
   ob_end_flush(); // end output buffering and flush it from memory
                   // not required but good practice to do so.
 
-  // this example was from a pretty old course runnign php 5.4
+  // this example was from a pretty old course running php 5.4
   // it doenst seem to be required in php 8.0+
 ?>
