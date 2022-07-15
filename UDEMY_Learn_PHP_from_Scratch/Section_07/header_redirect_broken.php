26e27dea5146e99e15138208b6492885aa2cbc0d
Richard A. Forth <richard.forth@gmail.com>
2022-07-15 20:09:34 +0000

    Updated commentary

--- UDEMY_Learn_PHP_from_Scratch/Section_07/header_redirect_broken.php
+++ UDEMY_Learn_PHP_from_Scratch/Section_07/header_redirect_broken.php
@@ -1,5 +1,8 @@
 <h4>Hello world!</h4>
 
 <?php
-  header('Location: http://www.google.co.uk');
+// having output before the header redirect was supposed to break in php 5.4,
+// producing a "Cannot modify headers" error message,  but when I tested this
+// in php 8.0.12 this is actually working for me.
+header('Location: http://www.google.co.uk');
 ?>
--- UDEMY_Learn_PHP_from_Scratch/Section_07/header_redirect_fixed.php
+++ UDEMY_Learn_PHP_from_Scratch/Section_07/header_redirect_fixed.php
@@ -8,4 +8,7 @@ ob_start(); // start output buffering
   header('Location: http://www.google.co.uk');
   ob_end_flush(); // end output buffering and flush it from memory
                   // not required but good practice to do so.
+
+  // this example was from a pretty old course runnign php 5.4
+  // it doenst seem to be required in php 8.0+
 ?>
