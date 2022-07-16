082abb7b1fabf5ba73a524f65725e0bf327302ca
Richard A. Forth <richard.forth@gmail.com>
2022-07-16 07:19:30 +0000

    Corrected a closing p tag

--- output_generators/html_form.php
+++ output_generators/html_form.php
@@ -1,6 +1,6 @@
 <textarea rows="10" cols="120"><?php 
 echo '<form action="" method="">
-<p>Name:<br><input type="text" name="name"><br><p/>
+<p>Name:<br><input type="text" name="name"><br></p>
 <p>Sex:<br> <input type="radio" name="sex" value="Male" /> Male <input type="radio" name="sex" value="Female" />Female</p>
 <p>Age:<br><input type="text" name="age"><br></p>
 <p>T&C Acceptance:<br><input type="checkbox" name="agree" />I agree with the Terms & Conditions.</p>
