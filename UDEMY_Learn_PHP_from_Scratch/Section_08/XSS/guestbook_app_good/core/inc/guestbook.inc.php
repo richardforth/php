e54ea636ecc5f666b4943bb25bffed7b94c5161e
Richard A. Forth <richard.forth@gmail.com>
2022-07-16 15:36:11 +0000

    Set characterset for htmlentities function

--- UDEMY_Learn_PHP_from_Scratch/Section_08/XSS/guestbook_app_good/core/inc/guestbook.inc.php
+++ UDEMY_Learn_PHP_from_Scratch/Section_08/XSS/guestbook_app_good/core/inc/guestbook.inc.php
@@ -3,7 +3,7 @@
 // adds a new post to the guestbook
 function add_post($message) {
     global $db;
-    $message = htmlentities($db->escape_string($message));
+    $message = htmlentities($db->escape_string($message), ENT_COMPAT, 'UTF-8');
     $db->query("insert into posts (post_body) values ('{$message}')");
 }
 
