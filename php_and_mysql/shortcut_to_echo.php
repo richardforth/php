5ad1cfc530bdd4b99f4251779a8098db36cc643c
Richard A. Forth <richard.forth@gmail.com>
2022-07-19 19:36:55 +0000

    Added shortcut to echo example

---
+++ php_and_mysql/shortcut_to_echo.php
@@ -0,0 +1,19 @@
+<?php
+  $name = "Richard";
+  $age = 44;
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Shortcut to echo</title>
+    </head>
+    <body>
+        <p>
+            My name is <?= $name ?>.
+        </p>
+        <p>
+            I am <?= $age ?> years old.
+        </p>
+    </body>
+</html>
